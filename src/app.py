8e5516f0cea486ca8349fbb2aca4108173fff967
Zerro <sachitnair2002@gmail.com>
2026-06-08 19:26:58 +0530

    Show sources in human readable format, allow for user to specify answer length

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from oracle import get_oracle_response
 
 st.title("IBM Licensing Assistant")
 
-st.subheader("IBM Licensing Expertise at your fingertips")
+st.caption("IBM Licensing Expertise at your fingertips")
 st.markdown("""
 
 *Get queries relating to IBM software products and licensing policies answered along with **citations to official documentations***
@@ -15,12 +15,14 @@ is_submitted = False
 
 form_data = {
     "query": None,
-    "query_category": None
+    "query_category": None,
+    "response_length": None,
 }
 
 with st.form(key="get_query", enter_to_submit=False):
     form_data['query'] = st.text_area(label="**Query**", placeholder="Write your query here", )
-    form_data['query_category'] = st.selectbox(label="Category", options=["Product information", "Licensing Scenario", "Other"])
+    form_data['query_category'] = st.selectbox(label="**Category**", options=["Product information", "Licensing Scenario", "General"])
+    form_data['response_length'] = st.selectbox(label="**Answer length**", options=['Summarized', 'Medium', 'Long'])
     is_submitted =  st.form_submit_button("Submit")
 
     if(is_submitted):
@@ -28,7 +30,8 @@ with st.form(key="get_query", enter_to_submit=False):
             st.warning("Please enter your query.")
 
 if is_submitted and form_data['query'] and form_data['query'].strip() != '':
-    response, sources = get_oracle_response(form_data["query"])
-    
-    st.text(response)
-    st.text(sources)
+    with st.spinner("Retreiving information..."):
+        response, sources = get_oracle_response(query=form_data["query"], response_length=form_data['response_length'], category=form_data['query_category'])
+
+    with st.container(border=True):
+        st.markdown(response)
--- src/oracle.py
+++ src/oracle.py
@@ -3,14 +3,21 @@ from dotenv import load_dotenv
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_chroma import Chroma
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 from langchain_core.output_parsers import StrOutputParser
 
 # import chromadb
 
 load_dotenv()
 
-def get_oracle_response(query: str):
+def get_oracle_response(query: str, response_length: str = "medium", category: str = "general"):
+
+    if not response_length:
+        response_length = "medium"
+
+    if not category:
+        category = "general"
+
     embeddings = GoogleGenerativeAIEmbeddings(
         model = "gemini-embedding-001",
         task_type = "retrieval_query",
@@ -25,10 +32,11 @@ def get_oracle_response(query: str):
     retriever = vector_db.as_retriever(search_kwargs={"k": 5})
 
     template = [
-        ("system", "You are an IBM Licensing expert who has been contracted to answer queries with regards to IBM software and its licensing. Use only the provided context to answer. "
-                    "If the answer is not in the context, state that clearly. "
-                    "Always cite the source and page number."),
-        ("human", "Context:\n{context}\n\nQuestion: {question}")
+        ("system", "You are an IBM Licensing expert who has been contracted to answer queries with regards to IBM software and its licensing. Use only the provided context to answer. You will receive inputs from the user such as the response length and the category of query: {category} to help you tailor your response"
+                    "Give your answer according to the requested length: {response_length}; If the user desires a summarized answer, use no more than 100 words, If the answer length is medium, then use no more than 300 words and if the answer length is long, then use no more than 500 words. However, ensure that the provided output answers the user's query."
+                    "If the answer is not in the context, state the following verbatim 'the information is not available'"
+                    "Always cite the source and page number in a pretty and human readable format, however NEVER reveal the folder structure etc. of the retrived information, just the name of the document and the page number (use markdown)."),
+        ("human", "Context:\n{context}\n\nQuestion: {question} \n\n Answer length desired: {response_length}")
     ]
     prompt = ChatPromptTemplate.from_messages(template)
 
@@ -46,16 +54,22 @@ def get_oracle_response(query: str):
 
     rag_chain = (
         {
-            "context": retriever | format_docs,
-            "question": RunnablePassthrough()
+            "context": (lambda x: x['question']) | retriever | format_docs,
+            "response_length": (lambda x: x['response_length']),
+            "question": (lambda x: x['question']),
+            "category": (lambda x: x['category'])
         }
         | prompt
         | llm
         | StrOutputParser()
     )
 
-    response = rag_chain.invoke(query)
-    print(vector_db)
+    response = rag_chain.invoke({
+        "question": query,
+        "response_length": response_length,
+        "category": category
+    })
+    # print(vector_db)
     source_documents = retriever.invoke(query)
     
     return response, source_documents
